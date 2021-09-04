97b77e80cb769e21e0346c6602decdd6bb2578bb
s3gm3ntationFault <36698770+s3gm3ntationFault@users.noreply.github.com>
2021-09-05 01:17:36 +0200

    Added recent posts component to the sidebar

---
+++ lib/screens/home/components/recent_posts.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../constraints.dart';
+import 'recent_post_card.dart';
+import 'sidebar_container.dart';
+
+class RecentPosts extends StatelessWidget {
+  const RecentPosts({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SidebarContainer(
+      title: "Recent Posts",
+      child: Column(
+        children: [
+          RecentPostCard(
+            image: "images/blog0.jpg",
+            title: "A geosynchronous pool table",
+            press: () {},
+          ),
+          SizedBox(height: kDefaultPadding),
+          RecentPostCard(
+            image: "images/blog1.jpg",
+            title: "The broken bottle related to another Harpoon",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
